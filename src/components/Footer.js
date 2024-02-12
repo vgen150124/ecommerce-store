cef24152e5ad2ace1672d29b3c9450717a7a03b9
vg150124 <virajg.encircle@gmail.com>
2024-02-12 15:35:57 +0530

    some changes in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -50,7 +50,7 @@ const Footer = () => {
                 <div>
                   <a
                     href="https://www.youtube.com/channel/UCwfaAHy4zQUb2APNOGXUCCA"
-                    target="_blank">
+                  >
                     <FaYoutube className="icons" />
                   </a>
                 </div>
