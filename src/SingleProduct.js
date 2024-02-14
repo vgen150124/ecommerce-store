61cc558da01d64a429da24c3f48464aae34d39a6
vg150124 <virajg.encircle@gmail.com>
2024-02-14 13:53:28 +0530

    useEffect depenency

--- src/SingleProduct.js
+++ src/SingleProduct.js
@@ -30,7 +30,7 @@ const SingleProduct = () => {
   
   useEffect(() => {
     getSingleProduct(`${API}?id=${id}`);
-  });
+  },[]);
 
   if (isSingleLoading) {
     return <div className="page_loading">Loading.....</div>;
